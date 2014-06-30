bbfc6117a2f4948df4d60a074604089fc94935eb
Rémy Hannequin <remy.hannequin@gmail.com>
2014-06-30 23:13:37 +0200

    Update sample SQL

--- db/places.sql
+++ db/places.sql
@@ -70,12 +70,12 @@ CREATE TABLE `place` (
 --
 
 INSERT INTO `place` (`id`, `name`, `address`, `description`, `latitude`, `longitude`, `town_id`) VALUES
-(1, 'Tour Eiffel', 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris', 'Badass Tower.', 48.8584, 2.29448, 3),
-(2, 'ESGI', '21 rue Erard', 'YOLO school', 48.8461, 2.38548, 3),
-(3, 'Temple Bar', '16 Temple Bar', 'Cool brews', 53.345176, -6.265716, 5),
-(4, 'Cuba Libre', '3B River Valley Rd', 'Come lah!', 1.291023, 103.845278, 6),
-(5, 'Beaugrenelle', '35 quai André Citroën', 'L\'ESGI, mais loin', 48.847339, 2.279451, 3),
-(6, 'Glasgow Science Centre', '50 Pacific Quay', 'Scotish Geode', 55.858542, -4.293802, 7);
+(1, 'Tour Eiffel', 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris', 'Badass Tower.', 48.8584, 2.29448, 1),
+(2, 'ESGI', '21 rue Erard', 'YOLO school', 48.8461, 2.38548, 1),
+(3, 'Temple Bar', '16 Temple Bar', 'Cool brews', 53.345176, -6.265716, 2),
+(4, 'Cuba Libre', '3B River Valley Rd', 'Come lah!', 1.291023, 103.845278, 3),
+(5, 'Beaugrenelle', '35 quai André Citroën', 'L\'ESGI, mais loin', 48.847339, 2.279451, 1),
+(6, 'Glasgow Science Centre', '50 Pacific Quay', 'Scotish Geode', 55.858542, -4.293802, 4);
 
 -- --------------------------------------------------------
 
@@ -99,10 +99,10 @@ CREATE TABLE `town` (
 --
 
 INSERT INTO `town` (`id`, `name`, `population`, `country_id`) VALUES
-(3, 'Paris', 2211000000, 1),
-(5, 'Dublin', 527612, 2),
-(6, 'Singapore', 5312000000, 3),
-(7, 'Glasgow', 598830, 4);
+(1, 'Paris', 2211000000, 1),
+(2, 'Dublin', 527612, 2),
+(3, 'Singapore', 5312000000, 3),
+(4, 'Glasgow', 598830, 4);
 
 --
 -- Contraintes pour les tables exportées
